45e228d544abbc4af3cc2807d59aee878ba3bc56
rbshakhtour <rbshakhtour@dons.usfca.edu>
2024-02-27 17:26:27 -0800

    almost done

--- server/db.js
+++ server/db.js
@@ -3,10 +3,25 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
     user: "postgres",
     password: "20611186",
-    host: "localhost",
+    host: "127.0.0.1",
     port: 5432, 
     database: "perntodo"
 });
 
+const createTableQuery = `
+    CREATE TABLE IF NOT EXISTS todo (
+        todo_id SERIAL PRIMARY KEY,
+        description VARCHAR(255)
+    )
+`;
+
+pool.query(createTableQuery, (err, res) => {
+    if (err) {
+        console.error('Error creating table:', err);
+    } else {
+        console.log('Table created successfully');
+        // Start your Node.js application here
+    }
+});
 
 module.exports = pool; 
--- server/index.js
+++ server/index.js
@@ -39,6 +39,27 @@ app.get("/todos/:id", async(req,res) =>{
   }
 });
 
+app.put("/todos/:id", async(req,res) =>{
+  try{
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id  $2",[description, id]);
+    res.json("todo was updated");
+  } catch(err) {
+    console.error(err.message);
+  }
+});
+
+app.delete("/todos/:id", async(req,res) =>{
+  try{
+    const { id } = req.params;
+    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [id]);
+    res.json("todo was deleted");
+  } catch(err) {
+    console.error(err.message);
+  }
+});
+
 // starting the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
